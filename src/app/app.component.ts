6a966c520913565a680b369bc31ed91edbe7055c
stephane0614 <nsimba.stephane033@gmail.com>
2022-11-28 02:55:48 +0100

    pokemon_object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
+  template: `<h1>LIST DE POKEMON</h1>
       
     <router-outlet></router-outlet>
-  `,
-  styles: []
+  `
+  
 })
-export class AppComponent {
-  title = 'pokemon_app';
+export class AppComponent implements OnInit{
+  pokemonList: Pokemon[] = POKEMONS;
+
+  ngOnInit() {
+
+    console.table(this.pokemonList);
+    this.selectpokemon(this.pokemonList[6])
+
+  }
+  selectpokemon(pokemon: Pokemon)
+  {
+    console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`);
+
+  }
 }
